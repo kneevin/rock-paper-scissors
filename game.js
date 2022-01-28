7e7fd19580d5ae754259ee5507da42be25da0e52
Kevin Le <kevin.k.le.2020@gmail.com>
2022-01-28 13:20:19 -0600

    Completed the logic of game.js. Also implemented a few more functions that either help decided the game or announce the game.

--- game.js
+++ game.js
@@ -13,28 +13,33 @@ function computerChoice() {
     return choices[randChoiceInt()];
 }
 
-function gameLogic(choice1, choice2) {
+function capitalize(str) {
+    // capitalizes the first letter
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
     // the choices must be passed in all lowercases!
     // returns the following values:
     // 0 -> tie
     // -1 -> 1st choice loses
     // 1 -> 1st choice wins
     // nested switch case
-    switch (choice1) {
+    switch (playerSelection) {
         case 'rock': // if first choice rock
-            switch (choice2) {
+            switch (computerSelection) {
                 case 'rock': return 0; // rock v rock -> tie
                 case 'paper': return -1; // rock v paper -> loss
                 case 'scissors': return 1; // rock v scissors -> wins
             }
         case 'paper': // if first choice paper
-            switch (choice2) {
+            switch (computerSelection) {
                 case 'rock': return 1; // paper v rock -> win
                 case 'paper': return 0; // paper v paper -> tie
                 case 'scissors': return -1; // paper v scissors -> loss
             }
         case 'scissors': // if first choice scissors
-            switch (choice2) {
+            switch (computerSelection) {
                 case 'rock': return -1; // scissors v rock -> loss
                 case 'paper': return 1; // scissors v paper -> win
                 case 'scissors': return 0; // scissors v scissors -> tie
@@ -42,6 +47,30 @@ function gameLogic(choice1, choice2) {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    let playerChoice = prompt('Enter your choice!');
+function announce(playerSelection, computerSelection, outcome) {
+    switch (outcome) {
+        case -1:
+            return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`;
+        case 0:
+            return `It's a Tie! You both chose ${playerSelection}...`;
+        case 1:
+            return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelect = prompt('Enter you choice').toLowerCase();
+        let computerSelect = computerChoice();
+        let result = playRound(playerSelect, computerSelect);
+        console.log(announce(playerSelect, computerSelect, result));
+
+        if (result > 0) { playerScore++; }
+        else if (result < 0) { computerScore++; }
+        else { tieScore++; }
+    }
+    console.log(`Player: ${playerScore} | Computer: ${computerScore} | Ties: ${tieScore}`);
 }
